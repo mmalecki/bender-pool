c390f8f74a26a5a0056f16a98b266eb3173e0de0
Maciej Małecki <me@mmalecki.com>
2014-02-19 11:51:38 +0100

    Fix simple test

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var cb = require('assert-called');
 var Bender = require('bender-crdt');
-var bender = new Bender({ ttl: 1000 });
+var bender = new Bender({ ttl: 1 });
 var createPool = require('../');
 
 var app = {
@@ -14,12 +14,12 @@ var app = {
 var c = { foo: 'bar' };
 
 function connect(reg, cb) {
-  assert.equal(reg, appReg, 'registration should be passed to connect handler');
+  assert.equal(reg.app, app.app, 'registration should be passed to connect handler');
   cb(null, c);
 }
 
 function disconnect(reg, conn, cb) {
-  assert.equal(reg, appReg, 'registration should be passed to disconnect handler');
+  assert.equal(reg.app, app.app, 'registration should be passed to disconnect handler');
   assert.equal(conn, c, 'pool item should be passed to disconnect handler');
 }
 
